786563d027783f8ac0e9b8e230a37760216ace75
CandyPopsWorld <108881546+CandyPopsWorld@users.noreply.github.com>
2025-03-10 00:10:46 +0300

    Create reports page

--- src/ui/components/cards/report-card/report-card.component.ts
+++ src/ui/components/cards/report-card/report-card.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {SolidButtonComponent} from "../../buttons/solid-button/solid-button.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {ReportCard} from "../../../../interfaces/data/reportCard.interface";
+import {ThemeColors} from "../../../../shared/const/colors/general/themeColors";
 
 @Component({
   selector: 'app-report-card',
   standalone: true,
-  imports: [],
+  imports: [
+    SolidButtonComponent,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './report-card.component.html',
   styleUrl: './report-card.component.css'
 })
 export class ReportCardComponent {
+  @Input() reportCard: ReportCard | null = null;
+  @Input() headerBgColor: keyof typeof ThemeColors | string = ThemeColors.Transparent;
+  @Input() mainBgColor: keyof typeof ThemeColors | string = ThemeColors.Transparent;
+  @Input() borderColor: keyof typeof ThemeColors | string = ThemeColors.Transparent;
 }
--- src/ui/pages/vault-page/tabs-content/reports-tab-content/reports-tab-content.component.ts
+++ src/ui/pages/vault-page/tabs-content/reports-tab-content/reports-tab-content.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {ReportService} from "../../../../../services/report.service";
+import {ReportCard} from "../../../../../interfaces/data/reportCard.interface";
+import {ReportCardComponent} from "../../../../components/cards/report-card/report-card.component";
+import {HeaderDescriptionComponent} from "../../../../components/text/header-description/header-description.component";
 
 @Component({
   selector: 'app-reports-tab-content',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    ReportCardComponent,
+    HeaderDescriptionComponent
+  ],
   templateUrl: './reports-tab-content.component.html',
   styleUrl: './reports-tab-content.component.css'
 })
 export class ReportsTabContentComponent {
+  reportCards: ReportCard[] = ReportService.getAllReports();
 
+  ngOnInit(): void {
+    this.reportCards = ReportService.getAllReports();
+  }
 }
