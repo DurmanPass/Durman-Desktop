743f880142b70c5405400bcfdf675c6aeb460526
CandyPopsWorld <108881546+CandyPopsWorld@users.noreply.github.com>
2025-03-10 23:57:11 +0300

    Create header account tab component

--- src/ui/pages/vault-page/tabs-content/account-tab-content/account-tab-content.component.ts
+++ src/ui/pages/vault-page/tabs-content/account-tab-content/account-tab-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderDescriptionComponent} from "../../../../components/text/header-description/header-description.component";
 
 @Component({
   selector: 'app-account-tab-content',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderDescriptionComponent
+  ],
   templateUrl: './account-tab-content.component.html',
   styleUrl: './account-tab-content.component.css'
 })
